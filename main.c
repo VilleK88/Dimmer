05bced782da476eef218f3593d7b67c53842b204
ville <ville.karppinen88@gmail.com>
2025-10-23 12:54:49 +0300

    Ex updated

--- main.c
+++ main.c
@@ -26,7 +26,7 @@ uint clamp(int br);
 int main() {
     const uint buttons[] = {SW2, SW1, SW0};
     const uint leds[] = {D1, D2, D3};
-    uint brightness = 500;
+    uint brightness = 0;
 
     // Initialize chosen serial port
     stdio_init_all();
@@ -55,17 +55,13 @@ int main() {
         if (lightsOn) {
             // Increase lighting
             if (!gpio_get(SW2)) {
-                printf("Brightness before: %d\r\n", brightness);
-                brightness = clamp((int)brightness - 40);
+                brightness = clamp((int)brightness - 50);
                 set_brightness(leds, brightness);
-                printf("Brightness after: %d\r\n", brightness);
             }
             // Decrease lighting
             if (!gpio_get(SW0)) {
-                printf("Brightness before: %d\r\n", brightness);
-                brightness = clamp((int)brightness + 40);
+                brightness = clamp((int)brightness + 50);
                 set_brightness(leds, brightness);
-                printf("Brightness after: %d\r\n", brightness);
             }
         }
 
@@ -84,10 +80,9 @@ void ini_buttons(const uint *buttons) {
 
 void ini_leds(const uint *leds, const uint brightness) {
     for (int i = 0; i < 3; i++) {
-        gpio_set_function(leds[i], GPIO_FUNC_PWM);
         const uint slice_num = pwm_gpio_to_slice_num(leds[i]); // Get slice
         const uint chan = pwm_gpio_to_channel(leds[i]); // Channel GPIO pin
-        //pwm_set_enabled(slice_num, false); // Stop PWM
+        pwm_set_enabled(slice_num, false); // Stop PWM
 
         pwm_config config = pwm_get_default_config(); // Get default PWM configuration
         pwm_config_set_clkdiv_int(&config, FREQ); // Set clock divider
@@ -95,8 +90,8 @@ void ini_leds(const uint *leds, const uint brightness) {
 
         pwm_init(slice_num, &config, false); // start set to false
         pwm_set_chan_level(slice_num, chan, brightness); // duty cycle
-        //gpio_set_function(leds[i], GPIO_FUNC_PWM);
-        pwm_set_enabled(leds[i], true); // Start PWM
+        gpio_set_function(leds[i], GPIO_FUNC_PWM); // Select PWM mode for pin
+        pwm_set_enabled(slice_num, true); // Start PWM
     }
 }
 
@@ -106,13 +101,10 @@ bool light_switch(const uint *leds, const uint brightness, const bool on) {
         const uint slice_num = pwm_gpio_to_slice_num(leds[i]);
         const uint chan = pwm_gpio_to_channel(leds[i]);
         pwm_set_chan_level(slice_num, chan, brightness);
-        if (on) pwm_set_enabled(slice_num, on);
     }
-    if (on) {
-        printf("Lights on\r\n");
+    if (on)
         return true;
-    }
-    printf("Lights off\r\n");
+
     return false;
 }
 
